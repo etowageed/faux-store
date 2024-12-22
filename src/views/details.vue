07fa146a797d5607e8dec926b47c696e758a28f2
etowageed <etowagideon@gmail.com>
2024-12-22 01:49:01 +0000

    started lightbox

--- src/views/details.vue
+++ src/views/details.vue
@@ -3,18 +3,24 @@ import axios from 'axios';
 import BtnSolid from '@/components/BtnSolid.vue';
 import Header from '@/components/Header.vue'
 import BtnCounter from '@/components/BtnCounter.vue';
+import BtnClear from '@/components/BtnClear.vue';
 
 
 export default {
 
     components: {
-        BtnSolid, Header, BtnCounter,
+        BtnSolid, Header, BtnCounter, BtnClear,
     },
     data() {
         return {
             product: {},
             cartItems: [],
             count: 1,
+            isActive: false,
+            lightBoxImgs: [
+
+            ],
+            currentimageIndex: 0,
 
         }
     },
@@ -25,7 +31,10 @@ export default {
             axios.get(`https://fakestoreapi.com/products/${id}`)
                 .then((res) => {
                     console.log(res.data)
-                    this.product = res.data
+                    this.product = res.data;
+
+                    // since we have just one image per product and want to duplicate into 4 on the lightbox
+                    this.lightBoxImgs = Array(4).fill(this.product.image)
                 })
                 .catch((error) => {
                     console.log(`something's not right`, error)
@@ -71,6 +80,28 @@ export default {
             this.$router.go(-1)
         },
 
+        toggleLightBox() {
+            this.isActive = !this.isActive
+        },
+
+        toggleLightBoxDiv() {
+            this.isActive = !this.isActive;
+
+
+        },
+        nextImage() {
+            if (this.currentimageIndex < this.lightBoxImgs.length - 1) {
+                this.currentimageIndex++;
+            }
+        },
+        prevImage() {
+            if (this.currentimageIndex > 0) {
+                this.currentimageIndex--;
+            }
+        }
+
+
+
     },
 
     created() {
@@ -92,7 +123,15 @@ export default {
     <Header />
     <div class="lg:w-1/3 mx-auto md:flex border border-red-500">
         <div class="md:w-1/3 lg:w-1/2">
-            <img :src="product.image" alt="" srcset="" class="w-full ">
+            <img :src="product.image" alt="" srcset="" class="w-full cursor-pointer" @click="toggleLightBox()">
+
+            <div class=" flex justify-between">
+                <img :src="product.image" alt="" srcset="" class=" h-14 w-14 cursor-pointer" @click="toggleLightBox()">
+                <img :src="product.image" alt="" srcset="" class=" h-14 w-14 cursor-pointer" @click="toggleLightBox()">
+                <img :src="product.image" alt="" srcset="" class=" h-14 w-14 cursor-pointer" @click="toggleLightBox()">
+                <img :src="product.image" alt="" srcset="" class=" h-14 w-14 cursor-pointer" @click="toggleLightBox()">
+
+            </div>
         </div>
 
         <div class="lg:w-1/2 flex flex-col justify-between">
@@ -106,6 +145,19 @@ export default {
 
         </div>
     </div>
+    <!-- lightbox -->
+
+    <div :class="{ lightbox: !isActive }" class="fixed flex z-50 top-0 w-full h-full bg-neutral-600 opacity-95"
+        @click="toggleLightBoxDiv()">
+
+        <!-- click.stop prevents the event from the image propagation to the parent lightbox div -->
+        <img :src="lightBoxImgs[currentimageIndex]" class="w-80 h-80 mx-auto my-auto" @click.stop>
+
+        <div @click.stop>
+            <BtnClear btnText="prev" @click="prevImage()" :aria-disabled="currentimageIndex === 0" />
+            <BtnClear btnText="next" @click="nextImage()" :disabled="currentimageIndex === lightBoxImgs.length - 1" />
+        </div>
+    </div>
 
 
 
@@ -113,4 +165,9 @@ export default {
 
 
 
-<style></style>
+<style>
+.lightbox {
+    display: none;
+
+}
+</style>
