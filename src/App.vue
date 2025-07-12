ed72d19c88b137abb7185d40518f92fa8633cf30
etowageed <etowagideon@gmail.com>
2025-07-13 00:04:20 +0100

    checks

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue'
 import ToastManager from './components/ToastManager.vue';
 export default {
